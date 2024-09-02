3e2aaeb9a44ca30f618e0579c44407c01d3549f9
erik-ng-3006 <erikng3006@gmail.com>
2024-09-02 17:34:45 +0700

    add hover effect

--- components/projects.tsx
+++ components/projects.tsx
@@ -11,34 +11,23 @@ const Projects = ({ projects }: { projects: ProjectMetaData[] }) => {
         return (
           <li
             key={project.slug}
-            className='grayscale transition-all duration-300 hover:grayscale-0'
+            className='relative grayscale transition-all duration-300 hover:grayscale-0'
           >
-            <h2>{project.title}</h2>
             <Link href={`/projects/${project.slug}`}>
               {project.image && (
-                <div className='h-72 w-full overflow-hidden bg-muted sm:h-60'>
+                <div className='relative h-72 w-full overflow-hidden bg-muted sm:h-60'>
                   <Image
                     src={project.image}
                     alt={project.title || ''}
-                    className='rounded-lg object-cover object-center transition-all'
-                    fill
+                    className='rounded-lg object-cover object-center transition-all hover:blur-sm'
+                    layout='fill'
                   />
                 </div>
               )}
-              {/*  <div className='absolute inset-[-1px] rounded-lg'></div>
-              <div className='absolute inset-x-0 bottom-0 translate-y-2 px-6 py-5'>
-                <h2 className='title line-clamp-1 text-xl no-underline'>
-                  {project.title}
-                </h2>
-                <p className='line-clamp-1 text-sm font-light text-muted-foreground'>
-                  {project.summary}
-                </p>
+              <div className='absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-center text-white opacity-0 transition-opacity duration-300 hover:opacity-100'>
+                <h2 className='text-xl'>{project.title}</h2>
+                <p className='mt-2 text-sm'>{project.summary}</p>
               </div>
-              {project.date && (
-                <p className='mt-1 text-sm font-light text-muted-foreground'>
-                  {formatDate(project.date)}
-                </p>
-              )} */}
             </Link>
           </li>
         )
