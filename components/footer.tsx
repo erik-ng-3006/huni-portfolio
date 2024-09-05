72308611a3d982e296431891fb6bdcc233c00bbf
erik-ng-3006 <erikng3006@gmail.com>
2024-09-05 08:56:48 +0700

    add new todos app project

--- components/footer.tsx
+++ components/footer.tsx
@@ -52,7 +52,7 @@ const Footer = () => {
                 target='_blank'
                 key={item.name}
                 href={item.href}
-                className='text-gray-400 hover:text-gray-500'
+                className='text-gray-400 hover:text-blue-600'
               >
                 <span className='sr-only'>{item.name}</span>
                 <item.icon className='h-6 w-6' aria-hidden='true' />
