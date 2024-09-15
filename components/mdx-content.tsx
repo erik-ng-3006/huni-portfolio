b992a183d6649cd553a544a5ed953611e4cfc6e4
erik-ng-3006 <erikng3006@gmail.com>
2024-09-15 11:29:33 +0700

    fix errors

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -4,9 +4,9 @@ import { highlight } from 'sugar-high'
 import Counter from '@/components/counter'
 import Alert from './alert'
 import Quiz from './quiz'
-import SchemaVisualizer from './schema-visualizer'
+//import SchemaVisualizer from './schema-visualizer'
 import ExpandableSection from './expandable-section'
-import InteractiveCodeSnippet from './interactive-code-snippet'
+//import InteractiveCodeSnippet from './interactive-code-snippet'
 function Code({ children, ...props }: any) {
   let codeHTML = highlight(children)
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
